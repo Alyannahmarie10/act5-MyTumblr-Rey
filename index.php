b86e46c12ce1cedc562879d56c466e49449fa576
ReyAlyannahMarie <reyalyannah10@gmail.com>
2022-04-21 00:24:20 +0800

    php login script added

--- index.php
+++ index.php
@@ -1,3 +1,42 @@
+<?php
+//start the session function
+session_start();
+
+//CREATE A PRE DEFINED USERNAME AND  without data base
+$acc_username = "AlyannahMarie";
+$acc_password = "101001yana";
+$acc_fullname = "Alyannah Marie Rey";
+$acc_adresss = "Hupi Santa Cruz Marinduque";
+
+//check the current url for the redirections later
+$url_add = "http://".$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'];
+
+//condition to know the button is cicked
+if (isset($_REQUEST['login_button']) === true){
+	// get the username and password from the form and compare to the predefined username and password
+     if($_REQUEST['form_username'] != $acc_username){
+     	header("Location: ".$url_add."?notexist");
+     }
+     else if ($_REQUEST['form_username'] === $acc_username && $_REQUEST['form_password'] != $acc_password){
+     	header("Location: ".$url_add."?wrongpassword");
+
+     }
+     else if ($_REQUEST['form_username'] == $acc_username && $_REQUEST['form_password'] == $acc_password){
+     	header("Location: ".$url_add."?success");
+
+     	// create a session variable
+     	$_SESSION['ses_username'] = $acc_username;
+     	$_SESSION['ses_password'] = $acc_password;
+     	$_SESSION['ses_fullname'] = $acc_fullname;
+     	$_SESSION['ses_address'] = $acc_adresss;
+
+     }
+}// end of log in button
+
+?>
+
+
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -24,12 +63,38 @@
 		      	</div>
 		      	<h3 class="text-center mb-4">MyTumblr Login</h3>
 						
-						<form action="#" class="login-form">
+						<form method="POST" class="login-form">
+
 		      		<div class="form-group">
-		      			<input type="text" class="form-control rounded-left" placeholder="Username" required>
+		      			
+		      			<?php
+		      			if (isset($_REQUEST['notexist']) === true){
+		      			      echo "<div class='alert alert-danger' role='alert'> Username does not exist... </div>";
+		      			} else if (isset($_REQUEST['wrongpassword']) === true){
+		      				    echo "<div class='alert alert-warning' role='alert'> Incorrect Password... </div>";
+
+		      			}else if (isset($_REQUEST['success']) === true){
+		      				echo "<div class='alert alert-sucess' role='alert'> Redirecting... </div>";
+		      				header("Refresh: 5; url=account.php");
+
+
+		      			}else if (isset($_REQUEST['logout']) === true){
+		      				echo "<div class='alert alert-info' role='alert'> Thankyou... </div>";
+
+		      			}else if (isset($_REQUEST['logfirst']) === true){
+		      				echo "<div class='alert alert-info' role='alert'> Please Login First... </div>";
+		      				
+		      			}else if (isset($_SESSION['ses_username']) === true){
+		      				echo "<div class='alert alert-warning' role='alert'> you are still logged in. Please <a href='> account.php click here </a> to proceed... </div>";
+		      			}
+		      			
+		      			?>
+
+		      			<input type="text" class="form-control rounded-left" placeholder="Username" name="form_username" required>
 		      		</div>
 	            <div class="form-group d-flex">
-	              <input type="password" class="form-control rounded-left" placeholder="Password" required>
+
+	              <input type="password" class="form-control rounded-left" placeholder="Password" name="form_password" required>
 	            </div>
 	            <div class="form-group d-md-flex">
 	            	<div class="w-50">
@@ -43,7 +108,7 @@
 								</div>
 	            </div>
 	            <div class="form-group">
-	            	<button type="submit" class="btn btn-primary rounded submit p-3 px-5">Get Started</button>
+	            	<button type="submit" class="btn btn-primary rounded submit p-3 px-5" name="login_button">Get Started</button>
 	            </div>
 	          </form>
 	        </div>
@@ -52,10 +117,6 @@
 		</div>
 	</section>
 
-  <!--<script src="js/jquery.min.js"></script>
-  <script src="js/popper.js"></script>
-  <script src="js/bootstrap.min.js"></script>
-  <script src="js/main.js"></script> -->
 
 	</body>
 </html>
